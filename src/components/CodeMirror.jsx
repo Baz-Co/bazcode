67f2f70bb561d4dc149d9f67dbf57c47957a35b8
BazCo <sheh@bazco.de>
2023-01-04 10:14:52 -0600

    update CodeMirror component props

--- src/components/CodeMirror.jsx
+++ src/components/CodeMirror.jsx
@@ -8,24 +8,25 @@ import { githubDark } from "@uiw/codemirror-theme-github";
 
 import "./CodeMirror.css";
 
-const extensions = [markdown({ base: markdownLanguage, codeLanguages: languages })];
-const stateFields = { history: historyField };
-const serializedState = localStorage.getItem("howTheInternetWorks");
 
-export default function CodeMirrorWidget() {
+export default function CodeMirrorWidget({ subject = "codeMirror", show = false, serialize = true }) {
+  console.log(serialize)
   const editor = useRef();
+  const extensions = [markdown({ base: markdownLanguage, codeLanguages: languages })];
+  const stateFields = { history: historyField };
+  const serializedState = localStorage.getItem(`${subject}-serialized`);  
   const { setContainer } = useCodeMirror({
     container: editor.current,
     extensions: extensions,
     theme: githubDark,
-    value: localStorage.getItem("myValue") || "",
-    initialState: serializedState
+    value: subject && serialize ? localStorage.getItem(subject) || "" : "",
+    initialState: serialize && serializedState
       ? { json: JSON.parse(serializedState || ""), fields: stateFields }
       : undefined,
     onChange: (value, viewUpdate) => {
-      localStorage.setItem("myValue", value);
+      localStorage.setItem(subject, value);
       const state = viewUpdate.state.toJSON(stateFields);
-      localStorage.setItem("howTheInternetWorks", JSON.stringify(state));
+      localStorage.setItem(`${subject}-serialized`, JSON.stringify(state));
     },
     indentWithTab: true,
     autoFocus: true,
@@ -40,7 +41,7 @@ export default function CodeMirrorWidget() {
   return (
     <>
       <div id="copyToClipboard">Copy To Clipboard</div>
-      <div id="CodeMirror" style={{ display: "none" }} ref={editor} />
+      <div id="CodeMirror" style={{ display: show ? "block" : "none" }} ref={editor} />
     </>
   );
 }
