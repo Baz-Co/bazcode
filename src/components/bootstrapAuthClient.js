b5cd09adb9ef9f4793e2debdd79475e034af7f62
BazCo <sheh@bazco.de>
2023-01-11 12:58:07 -0600

    add auth0 as an universal authentication provider

---
+++ src/components/bootstrapAuthClient.js
@@ -0,0 +1,24 @@
+window.onload = async () => {
+  await auth0
+    .createAuth0Client({
+      domain: "dev-r7pa8n4k8hvjzdq8.us.auth0.com",
+      clientId: "FokQe67VIujMi3ynlOOAfUvgXf5eN0bD",
+      authorizationParams: {
+        // redirect_uri: window.location.origin
+        redirect_uri: "http://localhost:3000/profile",
+      },
+    })
+    .then(async (auth0Client) => {
+      window.aC = auth0Client;
+      const isAuthenticated = await auth0Client.isAuthenticated();
+      if (isAuthenticated) {
+        if(window.location.pathname === "/login") {
+          window.location.assign(`${window.location.origin}/profile`);
+        }
+      } else {
+        if(window.location.pathname !== "/login") {
+          window.location.assign(`${window.location.origin}/login`);
+        }
+      }
+    });
+};
---
+++ src/components/profile.js
@@ -0,0 +1,39 @@
+window.onload = async () => {
+  if (!window.aC) {
+    auth0
+      .createAuth0Client({
+        domain: "dev-r7pa8n4k8hvjzdq8.us.auth0.com",
+        clientId: "FokQe67VIujMi3ynlOOAfUvgXf5eN0bD",
+        authorizationParams: {
+          // redirect_uri: window.location.origin
+          redirect_uri: "http://localhost:3000/profile",
+        },
+      })
+      .then(async (auth0Client) => {
+        if (
+          location.search.includes("state=") &&
+          (location.search.includes("code=") ||
+            location.search.includes("error="))
+        ) {
+          await auth0Client.handleRedirectCallback();
+          window.history.replaceState({}, document.title, "/profile");
+        }
+        window.aC = auth0Client;
+        const isAuthenticated = await auth0Client.isAuthenticated();
+        if (isAuthenticated) {
+          const userProfile = await auth0Client.getUser();
+          const loggedInElement = document.getElementById("loggedIn");
+          loggedInElement.hidden = false;
+          const profileElement = document.getElementById("profile");
+          profileElement.innerHTML = `
+                <p>${userProfile.name}</p>
+                <img src="${userProfile.picture}" />
+              `;
+        } else {
+          window.location.assign(`${window.location.origin}/login`);
+        }
+      });
+  }
+
+  // console.log(window.aC)
+};
