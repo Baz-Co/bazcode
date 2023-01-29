dedb703aa4b89a0c17ccd5af44cfb8e2b32d7354
BazCo <sheh@bazco.de>
2023-01-29 12:41:31 -0600

    update linktree displays

--- src/components/LinktreeDisplay.jsx
+++ src/components/LinktreeDisplay.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect } from "react";
 
+const LinkTreeBranch = ({ item, idx }) => {
+  return (
+    <div key={idx} className="rounded-full bg-slate-400 shadow-lg py-4 mb-1 w-full text-center text-black truncate text-truncate">
+      <li key={idx}>
+        <a href={item.link} target="blank">{ item.title }</a>
+      </li>
+    </div>
+  )
+}
+
 export default function LinktreeDisplay({ list = [] }) {
   // console.log("LinktreeDisplay", list);
   useEffect(() => {
@@ -7,16 +17,17 @@ export default function LinktreeDisplay({ list = [] }) {
   }, [list]);
 
   return (
-    <>
-      <ul>
+    <div className="w-96 min-w-680 mx-auto">
+      <ul className="w-full">
       {list.map((item, idx) => {
-        return (
-          <li key={idx}>
-            <a href={item.link} target="blank">{ item.title }</a>
-          </li>
-        )
+        // return (
+        //   <li key={idx}>
+        //     <a href={item.link} target="blank">{ item.title }</a>
+        //   </li>
+        // )
+        return LinkTreeBranch({ item, idx });
       })}
     </ul>
-    </>
+    </div>
   );
 }
